2f3ae36bd92b89b027dfddc8e49d0c153709727d
Anton Matosov <anton.matosov@gmail.com>
2018-03-25 22:23:46 -0700

    Use fixed seed

--- examples/TestNEAT_xor.py
+++ examples/TestNEAT_xor.py
@@ -108,7 +108,8 @@ def getbest(i):
     g = NEAT.Genome(0, 3, 0, 1, False, NEAT.ActivationFunction.UNSIGNED_SIGMOID,
                     NEAT.ActivationFunction.UNSIGNED_SIGMOID, 0, params, 0)
     pop = NEAT.Population(g, params, True, 1.0, i)
-    pop.RNG.Seed(int(time.clock()*100))
+    # pop.RNG.Seed(int(time.clock()*100))
+    pop.RNG.Seed(1234)
 
     generations = 0
     for generation in range(1000):
