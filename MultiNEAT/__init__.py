98b89cf8c2d92fb9a8c4cfa0960477ee6c79611b
Anton Matosov <anton.matosov@gmail.com>
2018-04-25 22:25:14 -0700

    Add runner placeholders

--- MultiNEAT/__init__.py
+++ MultiNEAT/__init__.py
@@ -2,6 +2,8 @@ import time
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 from ._MultiNEAT import *
 from .viz import *
+from .runner import *
+from .gym_runner import *
 
 # Get all genomes from the population
 def GetGenomeList(pop):
---
+++ MultiNEAT/runner.py
@@ -0,0 +1,8 @@
+
+
+class Runner:
+  def __init__(self, genome, *args, **kwargs):
+    self.genome = genome
+
+  def run(self):
+    
